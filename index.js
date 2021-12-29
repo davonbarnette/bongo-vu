9016423f62dd07983c4bc1083a7ca154ce3ee46c
Davon Barnette <davonbarnette@gmail.com>
2021-12-29 18:12:30 -0500

    add express

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 if (process.env.NODE_ENV !== "production"){
     require("dotenv").config();
 }
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.send('Success')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 const DiscordConsumer = require("./consumers/discord");
 const SlackConsumer = require("./consumers/slack");
